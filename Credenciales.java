2b72f63fae37bace57b44c56b212f6413ebb51f7
LEOBAINI <leobaini@hotmail.com>
2019-07-12 14:59:53 -0300

    Agregando la clase credenciales.
    Obvio sin las credenciales =)

---
+++ Credenciales.java
@@ -0,0 +1,25 @@
+package MetodosSql;
+
+public class Credenciales {
+
+	public static String base_OjoHalconOperativo="XX";
+	public static String ip_OjoHalconOperativo="XXXXXX";
+	public static String usuario_OjoHalconOperativo="XXX";
+	public static String password_OjoHalconOperativo="XXX";
+	
+	public static String base_SGT;
+	public static String ip_SGT;
+	public static String usuario_SGT;
+	public static String password_SGT;
+	
+	public static String base_Surveillance;
+	public static String ip_Surveillance;
+	public static String usuario_Surveillance;
+	public static String password_Surveillance;	
+	
+	
+	
+	
+	
+	
+}
